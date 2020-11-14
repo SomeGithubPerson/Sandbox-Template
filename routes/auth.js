4944d7945afe9c538e69a983d36d826ff68474f9
Shigero Toura <notshigetora@gmail.com>
2020-11-14 12:55:26 +0100

    Switch to mongodb temporarily

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,14 @@ const app = require("express").Router();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path')
-
 const bodyParser = require('body-parser');
-var sqlite3 = require('sqlite3').verbose();
+const mongoose = require('mongoose')
 console.log(__dirname)
-var db = new sqlite3.Database(`${__dirname}/../database/sandbox_db.db`, sqlite3.OPEN_READWRITE);
 app.get('/', (req, res)=> {
 
 })
 app.post('/register', (req, res)=> {
-   console.log('Starting serializing...')
-    db.serialize(function() {
-        console.log("Serializing done")
-        
-        db.run(`INSERT INTO user
-        VALUES (value1, value2, value3, ...);
-        `)
-    });
+   console.log('Linking to DB...');
 
 
 })
